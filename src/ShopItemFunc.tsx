54f6c2b6ed376036647c3a8c8a10b0a9c9ce0ed8
ns-morozova <ns_morozova@mail.ru>
2024-06-29 22:02:08 +0300

    Initial commit

---
+++ src/ShopItemFunc.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+interface IShopItem {
+    brand: string;
+    title: string;
+    description: string;
+    descriptionFull: string;
+    price: number;
+    currency: string;
+}
+
+interface IShopItemFuncProps {
+    item: IShopItem;
+}
+
+export const ShopItemFunc: React.FC<IShopItemFuncProps> = ({ item }) => {
+    return (
+        <div className="main-content">
+            <h2>{item.brand}</h2>
+            <h1>{item.title}</h1>
+            <h3>{item.description}</h3>
+            <div className="description">{item.descriptionFull}</div>
+            <div className="highlight-window mobile">
+                <div className="highlight-overlay"></div>
+            </div>
+            <div className="divider"></div>
+            <div className="purchase-info">
+                <div className="price">{`${item.currency}${item.price.toFixed(2)}`}</div>
+                <button>Добавить в корзину</button>
+            </div>
+
+        </div>
+    )
+
+}
