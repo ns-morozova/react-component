f75ef13d958ce05c5073b5af66f605020b18c035
ns-morozova <ns_morozova@mail.ru>
2024-06-29 23:33:26 +0300

    Corrected

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,29 @@
 import './App.css'
+import { ShopItemFunc } from './components/ShopItemFunc';
+
+const item = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: "Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.",
+  price: 399.00,
+  currency: '£'
+};
 
 function App() {
-
-  interface IShopItem {
-    brand: string;
-    title: string;
-    description: string;
-    descriptionFull: string;
-    price: number;
-    currency: string;
-  }
-
-  interface IShopItemFuncProps {
-    item: IShopItem;
-  }
-
-  const ShopItemFunc: React.FC<IShopItemFuncProps> = ({ item }) => {
-    return (
-      <div className="main-content">
-        <h2 className="brand">{item.brand}</h2>
-        <h1 className="title">{item.title}</h1>
-        <h3 className="descr-short">{item.description}</h3>
-        <div className="description">{item.descriptionFull}</div>
-        <div className="highlight-window mobile">
-          <div className="highlight-overlay"></div>
-        </div>
-        <div className="divider"></div>
-        <div className="purchase-info">
-          <div className="price">{`${item.currency}${item.price.toFixed(2)}`}</div>
-          <button className="button">Добавить в корзину</button>
-        </div>
-
-      </div>
-    )
-  }
-
-  const item = {
-    brand: 'Tiger of Sweden',
-    title: 'Leonard coat',
-    description: 'Minimalistic coat in cotton-blend',
-    descriptionFull: "Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.",
-    price: 399.00,
-    currency: '£'
-  };
-
   return (
     <>
-    <ShopItemFunc item={item}/>
+    <article>
+        <div className="container">
+          <div className="background-element"></div>
+          <div className="highlight-window">
+            <div className='highlight-overlay'></div>
+          </div>
+          <div className="window">
+            <ShopItemFunc item={item} />
+          </div>
+        </div>
+    </article>
     </>
   )
 }
