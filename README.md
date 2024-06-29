756e5c2b189a834059ada31b7a6bbc8137fb8912
ns-morozova <ns_morozova@mail.ru>
2024-06-29 22:18:42 +0300

    Refactor

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-
+[![Build status](https://ci.appveyor.com/api/projects/status/4h98jt1swcm4mv3r?svg=true)](https://ci.appveyor.com/project/ns-morozova/react-component)
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
 import './App.css'
-import { ShopItemFunc } from './ShopItemFunc'
 
 function App() {
+
+  interface IShopItem {
+    brand: string;
+    title: string;
+    description: string;
+    descriptionFull: string;
+    price: number;
+    currency: string;
+  }
+
+  interface IShopItemFuncProps {
+    item: IShopItem;
+  }
+
+  const ShopItemFunc: React.FC<IShopItemFuncProps> = ({ item }) => {
+    return (
+      <div className="main-content">
+        <h2 className="brand">{item.brand}</h2>
+        <h1 className="title">{item.title}</h1>
+        <h3 className="descr-short">{item.description}</h3>
+        <div className="description">{item.descriptionFull}</div>
+        <div className="highlight-window mobile">
+          <div className="highlight-overlay"></div>
+        </div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+          <div className="price">{`${item.currency}${item.price.toFixed(2)}`}</div>
+          <button className="button">Добавить в корзину</button>
+        </div>
+
+      </div>
+    )
+
+  }
 
   const item = {
     brand: 'Tiger of Sweden',
